48f90fd4683f41b0f38979685e265492678c89fa
Fahad <fahad.45854@iqra.edu.com>
2024-11-19 22:04:11 +0500

    ASsignment6 initialized

---
+++ nextjs-slow-practice/assignment-6/src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Courses</li>
+                    <li>Services</li>
+                    <li>Achievement</li>
+                    <li>About Us</li>
+                    <li>Testominial</li>
+                </ul>
+            </div>
+            <div>
+                <button>Login</button>
+                <button>Sign Up</button>
+            </div>
+        </div >
+    )
+}
+
+export default Navbar
---
+++ nextjs-slow-practice/assignment-6/src/components/UpperNav/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const UpperNav = () => {
+    return (
+        <div>
+            <div>
+                <span>Phone Number: 956 742 455 678</span>
+                <span
+
+                ></span>
+                <span>Email: info@ddsgnr.com</span>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default UpperNav
