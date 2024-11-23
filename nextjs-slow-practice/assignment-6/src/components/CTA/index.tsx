c197b6ffdbbb996f294b0f0c06230e155aa9720a
Fahad <fahad.45854@iqra.edu.com>
2024-11-23 21:31:38 +0500

    CTA image view at FE

--- nextjs-slow-practice/assignment-6/src/components/CTA/index.tsx
+++ nextjs-slow-practice/assignment-6/src/components/CTA/index.tsx
@@ -1,22 +1,52 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CTA = () => {
+
+    const CTAData = [
+        { logo: 'pen-tool-2.svg', h1: 'Design & Development', p: '50+ Courses Available' },
+        { logo: 'volume-high.svg', h1: 'Marketing', p: '50+ Courses Available' },
+        { logo: 'link.svg', h1: 'Development', p: '50+ Courses Available' },
+        { logo: 'microphone.svg', h1: 'Communication', p: '50+ Courses Available' },
+        { logo: 'group.svg', h1: 'Digital Marketing', p: '50+ Courses Available' },
+        { logo: 'arrow-2.svg', h1: 'Self Development', p: '50+ Courses Available' },
+    ]
+
     return (
         <div>
             <h1>Explore Courses by Category</h1>
             <p>Discover a wide range of courses covering a variety of subjects, taught by expert instructors.</p>
-            <div
-                // className="flex justify-center items-center bg-primary"
+            {CTAData.map(v => {
+                return (
+                    <>
+                        <div
+                            className="flex justify-center bg-primary"
+                        >
+                            <div
+                                className="bg-white flex items-center"
+                            >
+                                <Image src={'/images/CTA/' + v.logo} alt={v.logo} width={100} height={100} />
+                            </div>
+                            <div>
+                                <h1
+                                    className="font-bold"
+                                >{v.h1}</h1>
+                                <p
+                                    className="text-base"
+                                >{v.p}</p>
+                            </div>
+                        </div>
+                    </>
+                )
+            })}
+            {/* <div
                 className="flex justify-center bg-primary"
-            // className="grid grid-flow-col"
             >
                 <div
-                    // className="bg-white h-full"
-                    // className="bg-white h-[100%]"
-                    className="bg-white"
-                // className="bg-white h-[100px]"
-                // className="bg-white h-screen"
-                >logo</div>
+                    className="bg-white flex items-center"
+                >
+                    logo
+                </div>
                 <div>
                     <h1
                         className="font-bold"
@@ -24,9 +54,9 @@ const CTA = () => {
                     <p
                         className="text-base"
                     >50+ Courses Available</p>
-                </div>
-            </div>
-        </div>
+                </div> */}
+        </div >
+        // </div >
     )
 }
 
