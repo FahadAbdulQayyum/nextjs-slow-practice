f2455a33030e3d7c3efcd8391949db215dfa8a75
Fahad <fahad.45854@iqra.edu.com>
2024-11-23 23:21:00 +0500

    removed comments

--- nextjs-slow-practice/assignment-6/src/components/CTA/index.tsx
+++ nextjs-slow-practice/assignment-6/src/components/CTA/index.tsx
@@ -17,38 +17,18 @@ const CTA = () => {
             <h1>Explore Courses by Category</h1>
             <p>Discover a wide range of courses covering a variety of subjects, taught by expert instructors.</p>
             <span
-                // className="grid grid-flow-row grid-rows-5"
-                // className="grid grid-flow-col grid-cols-3"
-                // className="grid grid-cols-3 gap"
-                // className="grid grid-cols-3 gap-4"
                 className="grid grid-cols-5 gap-4"
-            // className="grid grid-cols-3"
             >
                 {CTAData.map(v => {
                     return (
                         <div
-                            // className="flex justify-center bg-primary"
-                            // className="flex justify-start bg-primary py-4"
-                            // className="flex justify-start bg-primary"
-                            // className="flex justify-start bg-primary h-16"
                             className="flex justify-start bg-primary h-18"
-                        // className="grid"
-                        // className="grid grid-col-3"
                         >
                             <div
                                 className="flex items-center w-[40%]"
-                            // className="flex items-center"
-                            // className="bg-white flex items-center"
-                            // className="bg-white flex justify-start"
                             >
                                 <div
-                                    // className="h-[100%] bg-white w-[30%]"
-                                    // className="h-[100%] bg-white"
-                                    // className="flex justify-center h-[100%] bg-white px-5 m-3"
-                                    // className="flex justify-center  bg-white px-5 m-3"
                                     className="flex justify-center  bg-white px-5 py-3 m-3"
-                                // className="h-screen bg-white w-[30%]"
-                                // className="bg-white "
                                 >
                                     <Image src={'/images/CTA/' + v.logo} alt={v.logo} width={30} height={30} />
                                 </div>
@@ -67,24 +47,7 @@ const CTA = () => {
                     )
                 })}
             </span >
-            {/* <div
-                className="flex justify-center bg-primary"
-            >
-                <div
-                    className="bg-white flex items-center"
-                >
-                    logo
-                </div>
-                <div>
-                    <h1
-                        className="font-bold"
-                    >Design & Development</h1>
-                    <p
-                        className="text-base"
-                    >50+ Courses Available</p>
-                </div> */}
         </div >
-        // </div >
     )
 }
 
