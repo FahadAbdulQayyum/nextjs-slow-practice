d19415f67c06ecb4cc6acd2ae599aaf544e3ddcf
Fahad <fahad.45854@iqra.edu.com>
2024-11-23 17:50:43 +0500

    carousel implemented from shadcn

--- nextjs-slow-practice/assignment-6/src/components/Affiliation/index.tsx
+++ nextjs-slow-practice/assignment-6/src/components/Affiliation/index.tsx
@@ -9,12 +9,13 @@ import { Card, CardContent } from "@/components/ui/card"
 import Image from "next/image"
 
 const Affiliation = () => {
+    const carouselImages = ["carousel1.svg", "carousel2.svg", "carousel3.svg", "carousel5.svg", "carousel6.svg", "carousel7.svg"];
     return (
         <div>
             <div>
                 <h1>Trusted by 2000+ companies worldwide.</h1>
             </div>
-            <div>
+            <div className="w-full">
                 <Carousel
                     opts={{
                         align: "start",
@@ -25,14 +26,11 @@ const Affiliation = () => {
                         {Array.from({ length: 5 }).map((_, index) => (
                             <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                                 <div className="p-1">
-                                    <Card>
-                                        <CardContent className="flex aspect-square items-center justify-center p-6">
-                                            {/* <Image src={"/images/dsgnrlogo.svg"} alt="logo" width={100} height={100} /> */}
-                                            {/* <Image src={`/images/carousel{index+1}.svg`} alt="logo" width={100} height={100} /> */}
-                                            <Image src={`/images/carousel{1}.svg`} alt="logo" width={100} height={100} />
-                                            {/* <span className="text-3xl font-semibold">{index + 1}</span> */}
-                                        </CardContent>
-                                    </Card>
+                                    {/* <Card> */}
+                                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                                        <Image src={'/images/' + carouselImages[index + 1]} alt="logo" width={100} height={100} />
+                                    </CardContent>
+                                    {/* </Card> */}
                                 </div>
                             </CarouselItem>
                         ))}
